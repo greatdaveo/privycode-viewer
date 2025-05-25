30cf60afe33697deceb7863d4573331bb54b22e8
David Olowomeye <olowomeyedavid242@gmail.com>
2025-05-26 00:21:41 +0100

    Triggered a reload when the token is being reset

--- src/App.tsx
+++ src/App.tsx
@@ -16,6 +16,7 @@ export default function App() {
       localStorage.removeItem("github_token");
       localStorage.setItem("github_token", token);
       // window.history.replaceState({}, "", "/dashboard");
+      window.location.reload();
     }
   }, []);
 
