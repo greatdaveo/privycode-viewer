82113544400fc4f1f0d8aa04b7aa11983f6d970a
David Olowomeye <olowomeyedavid242@gmail.com>
2025-05-25 23:07:12 +0100

    Added the useAuth Hook to the protected route component

--- src/components/ProtectedRoute.tsx
+++ src/components/ProtectedRoute.tsx
@@ -1,31 +1,11 @@
-import { JSX, useEffect, useState } from "react";
+import { JSX } from "react";
 import { Navigate } from "react-router-dom";
-
-const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
+import useAuth from "../hooks/useAuth";
 
 const ProtectedRoute = ({ children }: { children: JSX.Element }) => {
-  const [loading, setLoading] = useState(true);
-  const [authenticated, setAuthenticated] = useState(false);
+  const { authenticated, checked } = useAuth();
 
-  useEffect(() => {
-    const checkAuth = async () => {
-      try {
-        const res = await fetch(`${BACKEND_URL}/me`, {
-          credentials: "include",
-        });
-
-        setAuthenticated(res.ok);
-      } catch (err) {
-        setAuthenticated(false);
-      } finally {
-        setLoading(false);
-      }
-    };
-
-    checkAuth();
-  }, []);
-
-  if (loading) return null;
+  if (!checked) return <p>Loading...</p>;
 
   if (!authenticated) {
     return <Navigate to="/?message=connect_github" replace />;
